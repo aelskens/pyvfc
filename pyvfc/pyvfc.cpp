3bfa68a1c9772ca1cfc129c67ca8eea27c7eb771
aelskens <arthurelsk@gmail.com>
2023-05-08 17:00:59 +0200

    test 3

--- pyvfc/pyvfc.cpp
+++ pyvfc/pyvfc.cpp
@@ -6,8 +6,8 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(pyvfc, m) {
     py::class_<VFC>(m, "VFC")
-        // .def(py::init())
-        .def(py::init<int, int, float, float, float, float, float, float, float>())
+        .def(py::init())
+        // .def(py::init<int, int, float, float, float, float, float, float, float>())
         // .def(py::init<int method, int maxIter, float gamma, float beta, float lambda, float theta, float a, float ecr, float minP>())
         .def("setData", &VFC::setData)
         .def("optimize", &VFC::optimize)
